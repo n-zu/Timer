1769e5a7e18d802991abce875845496b685ebf63
nicolaszulaica <nzulaica@fi.uba.ar>
2022-03-01 10:12:09 -0300

    update ignore step
    check for changes in src

--- scripts/ignore-step.sh
+++ scripts/ignore-step.sh
@@ -1,12 +1,15 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 
-if [[ "$VERCEL_ENV" == "production" ]] ; then
-  # Proceed with the build
-  echo "✅ - Build can proceed"
-  exit 1;
-
-else
-  # Don't build
-  echo "🛑 - Build cancelled"
+if ! [[ "$VERCEL_ENV" == "production" ]] ; then
+  echo "🛑 - Build cancelled - Not a production env"
   exit 0;
 fi
+echo "✅ - Production env"
+
+if ! [[ `git status --porcelain | grep "src/"` ]] ; then
+  echo "🛑 - Build cancelled - No changes in src"
+  exit 0;
+fi
+echo "✅ - Changes in src"
+
+exit 1;
